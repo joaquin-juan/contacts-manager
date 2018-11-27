843bca673ee2e0679070bcda3d650798217433da
J.R <juan.delarosa1224@gmail.com>
2018-11-27 10:24:56 -0600

    view contacts and addContacts funtional

--- src/ContactsApplication.java
+++ src/ContactsApplication.java
@@ -1,7 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ContactsApplication {
+	static String directory = "src/.";
+	static String filename = "contacts.txt";
 
+	static Path dataDirectory = Paths.get(directory);
+	static Path dataFile = Paths.get(directory, filename);
+	static Path p = Paths.get(directory, filename);
 
 
 	public static void main(String[] args) {
@@ -50,13 +63,28 @@ public class ContactsApplication {
 	}
 
 	public static void viewContacts() {
-
+//		System.out.println(p);
+		List<String> contacts = new ArrayList<>();
+		try {
+			contacts = Files.readAllLines(p);
+			for (int i = 0; i < contacts.size(); i += 1) {
+				System.out.println(contacts.get(i));
+			}
+			} catch(IOException e){
+				e.printStackTrace();
+				System.out.println(contacts);
+			}
 	}
 
 
 	public static void addNewContact() {
-
-
+		List<String> addContact = Arrays.asList("Joaquin | 1232101234", "Juan | 3242344567");
+	try {
+		Files.write(p, addContact, StandardOpenOption.APPEND);
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+		System.out.println(addContact);
 	}
 
 	public static void searchByName() {
