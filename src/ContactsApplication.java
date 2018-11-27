857d5910a88e5b19a000a162fd34d093595e1179
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-26 18:44:25 -0600

    fixed problem with user input registering an additional line before input is grabbed by the scanner. cleaned up the flow of the methods and deleted duplicate items.

--- src/ContactsApplication.java
+++ src/ContactsApplication.java
@@ -2,50 +2,53 @@ import java.util.Scanner;
 
 public class ContactsApplication {
 
-	static Scanner sc = new Scanner(System.in);
-	static Input in = new Input(sc);
+
 
 	public static void main(String[] args) {
-		System.out.format(showMenu());
-//		init();
-//		int testInt = in.getInt();
-//		System.out.println(testInt);
-		in.getInt();
-
+		init();
 	}
 
-	public static void init(String[] args) {
-
+	public static void init() {
+		menuApplication();
 	}
 
-	public static String showMenu() {
-	return ("1. View contacts.\n" +
-			"2. Add a new contact.\n" +
-			"3. Search a contact by name.\n" +
-			"4. Delete an existing contact.\n" +
-			"5. Exit.\n" +
-			"Enter an option (1, 2, 3, 4 or 5):");
 
-	}
 	public static void menuApplication() {
+
+		System.out.format(showMenu());
+
+		Scanner sc = new Scanner(System.in);
+		Input in = new Input(sc);
+		int userInput = in.getInt();
+
 //		we will use if/else statements
-	if (in.getInt() == 1) {
+	if (userInput == 1) {
 		viewContacts();
 		menuApplication();
-	} else if (in.getInt() == 2) {
+	} else if (userInput == 2) {
 		addNewContact();
 		menuApplication();
-	} else if (in.getInt() == 3) {
+	} else if (userInput == 3) {
 		searchByName();
 		menuApplication();
-	} else if (in.getInt() == 4) {
+	} else if (userInput == 4) {
 		deleteContact();
 		menuApplication();
-	} else if (in.getInt() == 5) {
+	} else if (userInput == 5) {
 		System.out.println("Goodbye");
 	}
 
 	}
+
+	public static String showMenu() {
+		return ("1. View contacts.\n" +
+				"2. Add a new contact.\n" +
+				"3. Search a contact by name.\n" +
+				"4. Delete an existing contact.\n" +
+				"5. Exit.\n" +
+				"Enter an option (1, 2, 3, 4 or 5):\n");
+	}
+
 	public static void viewContacts() {
 
 	}
